d0708bc694788104ce1b8b5ce0dc85aaa4e63068
Tanko Hunor <tanko_hunor@yahoo.com>
2019-02-04 09:43:48 +0200

    Added moment.js and calendar  functions for Week component

--- src/app/sec-iris2-week/sec-iris2-week.component.ts
+++ src/app/sec-iris2-week/sec-iris2-week.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-sec-iris2-week',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sec-iris2-week.component.css']
 })
 export class SecIris2WeekComponent implements OnInit {
+  now = moment()
+  weekDays = []
 
   constructor() { }
 
   ngOnInit() {
+    this.calculateWeekDays()
+  }
+
+
+
+  // Calendar functions
+
+  calculateWeekDays() {
+    let days = []
+    // Clone this.now
+    let day = moment(this.now).day(0)
+    for (let i=0; i<6; i++){
+      day.add(1, 'day')
+      days.push(day.format('DD-MM-YYYY').slice(0,10))
+    }
+    this.weekDays = days
+  }
+
+  backOneWeek() {
+    this.now.subtract(7, 'days')
+    this.calculateWeekDays()
+  }
+  
+  forwardOneWeek() {
+    this.now.add(7, 'days')
+    this.calculateWeekDays()
   }
 
 }
